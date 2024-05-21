996e030b8fc9f5ce331092b09dbba6e6de12776f
rkdtnlzl <rkdtnlzl@naver.com>
2024-05-21 22:21:51 +0900

    [Feat] 코드 기능 구현

--- BMI-Calculator/ViewController.swift
+++ BMI-Calculator/ViewController.swift
@@ -8,7 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: property
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var topIntroLabel: UILabel!
+    @IBOutlet var bottomIntroLabel: UILabel!
+    @IBOutlet var heightLabel: UILabel!
+    @IBOutlet var heightTextField: UITextField!
+    @IBOutlet var weightLabel: UILabel!
+    @IBOutlet var weightTextField: UITextField!
+    @IBOutlet var randomBMIButton: UIButton!
+    @IBOutlet var resultButton: UIButton!
+    
+    //MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
